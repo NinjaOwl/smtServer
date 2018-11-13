38e6dd8d7833ec0fe069adb11a3e0780c9b0e2a8
admin <70962450@qq.com>
2018-11-13 09:44:33 +0800

    api docs

--- web/docs/api_project.js
+++ web/docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-11-12T10:04:43.232Z",
+    "time": "2018-11-13T01:42:56.980Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
